6e7e534e0f4dea322147eb53e98ae4d7ab518065
fauzannursalma <fauzannursalmam@gmail.com>
2020-04-11 15:09:44 +0700

    pertemuan 9

---
+++ kuliah/pertemuan9/pw_193040053.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2020 at 11:35 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_193040053`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `jurusan` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `foto` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `jurusan`, `email`, `foto`) VALUES
+(1, 'Fauzan Nursalma Mawardi', '193040053', 'Teknik Informatika', '193040053@mail.unpas.ac.id', 'fauzan.jpeg'),
+(2, 'Muhammad Angga Saputra', '193040070', 'Teknik Informatika', '193040070@mail.unpas.ac.id', 'angga.jpeg'),
+(3, 'Herlan Nurachman', '193040043', 'Teknik Informatika', '193040043@mail.unpas.ac.id', 'herlan.jpeg'),
+(4, 'Muhammad Riskhi Ramadhan', '193060033', 'Perencanaan Wilayah dan Kota', '193060033@mail.unpas.ac.id', 'rama.jpeg'),
+(5, 'Muhammad Raihan Ramadhan', '193060045', 'Perencanaan Wilayah dan Kota', '193060045@mail.unpas.ac.id', 'raihan.jpeg'),
+(6, 'Hannan Fakhrul Hakim', '193040073', 'Teknik Informatika', '193040073@mail.unpas.ac.id', 'hannan.jpeg'),
+(7, 'Hilma Salsabila', '193010074', 'Teknik Industri', '193040074@mail.unpas.ac.id', 'hilma.jpeg'),
+(8, 'Gustano Subhantio', '193010026', 'Teknik Industri', '193010026@mail.unpas.ac.id', 'tio.jpeg'),
+(9, 'Gilda Agustina Pramesti', '193010067', 'Teknik Industri', '193010067@mail.unpas.ac.id', 'gilda.jpeg'),
+(10, 'Putri Amaliasikin', '193020026', 'Teknik Pangan', '193020026@mail.unpas.ac.id', 'putri.jpeg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
